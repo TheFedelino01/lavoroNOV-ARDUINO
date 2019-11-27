112777c38f99b171cde94fd27117df79bb6e61c8
colomboalessandro <57433038+colomboalessandro@users.noreply.github.com>
2019-11-27 12:35:30 +0100

    Update arduinoXProgetto.ino

--- Arduino/arduinoXProgetto.ino
+++ Arduino/arduinoXProgetto.ino
@@ -1,11 +1,12 @@
-void setup() {
-  Serial.begin(9600);
-
+int potentiometerPin = 0;
+int value = 0;
+void setup()
+{
+  Serial.begin(9600); // inizializzazione porta seriale
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("ciao");
-  delay(1000);    
-
-  }
+void loop()
+{
+  value = analogRead(potentiometerPin); // leggo valore potenziometro
+  Serial.println(value); // mando in seriale i dati
+  delay(300);//velocità
+}
